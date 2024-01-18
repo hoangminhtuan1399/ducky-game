1da43d63d4579b02514e25e68bf8fed46b41e818
Net <tuan.hm.2289@aptechlearning.edu.vn>
2024-01-18 21:22:03 +0700

    set up database url

--- src/main/java/mongodb/mongoDbConfig/MongoDbConfig.java
+++ src/main/java/mongodb/mongoDbConfig/MongoDbConfig.java
@@ -1,6 +1,6 @@
 package mongodb.mongoDbConfig;
 
 public interface MongoDbConfig {
-    String MONGO_URI = "mongodb://localhost:27017";
+    String MONGO_URI = "mongodb+srv://tuanhm:nmvQHZ82AK4lTajn@duckydatabase.62ks3pj.mongodb.net/";
     String DB_NAME = "demo";
 }
