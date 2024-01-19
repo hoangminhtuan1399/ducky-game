75f63277b98d43e7aba262799dfd135c5625cbfc
nguyen anh vu <vupot123@gmail.com>
2024-01-19 18:30:35 +0700

    test window duck

---
+++ src/main/java/main/Main2.java
@@ -0,0 +1,10 @@
+package main;
+
+import main.jade.Window;
+
+public class Main2 {
+    public static void main(String[] args) {
+        Window window = Window.get();
+        window.run();
+    }
+}
---
+++ src/main/java/main/jade/Window.java
@@ -0,0 +1,86 @@
+package main.jade;
+
+import org.lwjgl.Version;
+import org.lwjgl.glfw.GLFWErrorCallback;
+import org.lwjgl.opengl.GL;
+
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.system.MemoryUtil.NULL;
+
+public class Window {
+    private int width, height;
+    private String title;
+    private long glfwWindow;
+    private static Window window = null;
+
+    private Window() {
+        // Thiết lập giá trị mặc định cho chiều rộng, chiều cao, và tiêu đề cửa sổ
+        this.width = 1920;
+        this.height = 1080;
+        this.title = "duck";
+    }
+
+    public static Window get() {
+        // Triển khai mẫu Singleton để đảm bảo chỉ có một thể hiện của lớp Window
+        if (Window.window == null) {
+            Window.window = new Window();
+        }
+        return Window.window;
+    }
+
+    public void run() {
+        // In ra màn hình console thông báo chào mừng cùng phiên bản của LWJGL
+        System.out.println("Hello LWJGL " + Version.getVersion() + "!");
+
+        // Gọi phương thức khởi tạo và vòng lặp chính của ứng dụng
+        init();
+        loop();
+    }
+
+    public void init() {
+        // Tạo một callback để in ra thông báo lỗi trực tiếp ra màn hình console
+        GLFWErrorCallback.createPrint(System.err).set();
+
+        // Khởi tạo GLFW
+        if (!glfwInit()) {
+            throw new IllegalStateException("Unable to initialize GLFW");
+        }
+
+        // Đặt các giá trị mặc định cho cửa sổ GLFW
+        glfwDefaultWindowHints();
+        glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
+        glfwWindowHint(GLFW_MAXIMIZED, GLFW_TRUE);
+
+        // Tạo cửa sổ GLFW với chiều rộng, chiều cao, tiêu đề và giá trị NULL
+        glfwWindow = glfwCreateWindow(this.width, this.height, this.title, NULL, NULL);
+
+        // Kiểm tra nếu việc tạo cửa sổ không thành công
+        if (glfwWindow == NULL) {
+            throw new IllegalStateException("Failed to create the GLFW window");
+        }
+
+        // Đặt cửa sổ GLFW vừa tạo làm ngữ cảnh hiện tại
+        glfwMakeContextCurrent(glfwWindow);
+
+        // v-sync
+        glfwSwapInterval(1);
+
+        // làm cho cửa sổ hiển thị
+        glfwShowWindow(glfwWindow);
+        GL.createCapabilities();
+    }
+
+    public void loop() {
+        // Vòng lặp chính của ứng dụng, kiểm tra điều kiện đóng cửa sổ
+        while (!glfwWindowShouldClose(glfwWindow)) {
+            // Giao tiếp với cửa sổ GLFW: Đổi frame buffers và xử lý sự kiện
+            glfwPollEvents();
+            glClearColor(1.0f / 6.0f, 3.0f / 6.0f, 6.0f / 6.0f, 1.0f / 6.0f);
+            glClear(GL_COLOR_BUFFER_BIT);
+            glfwSwapBuffers(glfwWindow);
+
+        }
+    }
+}
