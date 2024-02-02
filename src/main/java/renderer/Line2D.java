2ba248389d56f79ca1b30affc132cc86d31e635f
bao long <pikalong0311@gmail.com>
2024-02-02 18:03:05 +0700

    add some new package, class and update according Physics playlist

--- src/main/java/renderer/Line2D.java
+++ src/main/java/renderer/Line2D.java
@@ -10,6 +10,10 @@ public class Line2D {
     private Vector3f color;
     private int lifetime;
 
+    public Line2D(Vector2f from, Vector2f to) {
+        this.from = from;
+        this.to = to;
+    }
     public Line2D(Vector2f from, Vector2f to, Vector3f color, int lifetime) {
         this.from = from;
         this.to = to;
@@ -30,8 +34,18 @@ public class Line2D {
     public Vector2f getTo() {
         return to;
     }
+    public Vector2f getStart() {
+        return this.from;
+    }
+
+    public Vector2f getEnd() {
+        return this.to;
+    }
 
     public Vector3f getColor() {
         return color;
+    }
+    public float lengthSquared() {
+        return new Vector2f(to).sub(from).lengthSquared();
     }
 }
